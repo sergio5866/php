52a4d325b116c394fcd86f9ec51c1d81118ad97c
sergio5866 <72936351+sergio5866@users.noreply.github.com>
2021-02-10 16:00:37 +0100

    version1

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+    <?php echo '<p>Prueba con PHP de pipelines CICD con Azure DevOps testeo del miercoles 10 febrero</p>'; ?>
+ </body>
+</html>
